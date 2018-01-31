f8b52048eedbfb5f7a444bf09f63a6e2571e8428
dkhansemand <dkhansemand@gmail.com>
2018-01-31 21:19:31 +0100

    delete query only executes if unlink === true

--- lib/Category-class.php
+++ lib/Category-class.php
@@ -95,8 +95,8 @@ class Category extends Database
             $mediaInfo = (new self)->query("SELECT categoryImage, `filename` FROM category 
                                                 INNER JOIN media ON categoryImage = mediaId 
                                                 WHERE categoryId = :ID", [':ID' => $categoryId])->fetch();
-            (new self)->query("DELETE FROM category WHERE categoryId = :ID", [':ID' => $categoryId]);
             if(unlink(dirname(__DIR__) . DIRECTORY_SEPARATOR . 'assets' . DIRECTORY_SEPARATOR . 'media' . DIRECTORY_SEPARATOR . $mediaInfo->filename)){
+                (new self)->query("DELETE FROM category WHERE categoryId = :ID", [':ID' => $categoryId]);
                 (new self)->query("DELETE FROM media WHERE mediaId = :ID", [':ID' => $mediaInfo->categoryImage]);
                 return true;
             }
