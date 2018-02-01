06edecfb441fcd735bc774dc343515feaa18100d
Andreas Holm Hansson <3941133@RTS-365.dk>
2018-02-01 08:51:53 +0100

    Categories done

--- views/categories.php
+++ views/categories.php
@@ -1,13 +1,15 @@
-<?php
-    $Categories = Category::GetCategoriesByType(Router::GetParam(':CATEGORY'));
-    
-?>
 <h3><?=ucfirst(Router::GetParam(':CATEGORY'))?></h3>
 <section id="categories">
-    <div id="category-item">
-        <a href="#Produkter">
-            <p><?=$Categories[0]->categoryName?></p>
-            <img src="<?=Router::$BASE?>assets/media/<?=$Categories[0]->filename?>" alt="<?=$Categories[0]->categoryName?>">
-        </a>
-    </div>
+    <?php
+        foreach(Category::GetCategoriesByType(Router::GetParam(':CATEGORY')) as $category){
+    ?>
+        <div id="category-item">
+            <a href="#Produkter">
+                <p><?=$category->categoryName?></p>
+                <img src="<?=Router::$BASE?>assets/media/<?=$category->filename?>" alt="<?=$category->categoryName?>">
+            </a>
+        </div>
+    <?php
+        }
+    ?>
 </section>
