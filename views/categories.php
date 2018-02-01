7d5ec6a2325fc257719a3e58c495f41736dec803
Andreas Holm Hansson <3941133@RTS-365.dk>
2018-02-01 08:43:32 +0100

    Categories grid layout done

--- views/categories.php
+++ views/categories.php
@@ -1,8 +1,13 @@
+<?php
+    $Categories = Category::GetCategoriesByType(Router::GetParam(':CATEGORY'));
+    
+?>
 <h3><?=ucfirst(Router::GetParam(':CATEGORY'))?></h3>
-    <pre>
-        <?php
-            var_dump(Router::GetParams());
-            var_dump(Category::GetCategoriesByType(Router::GetParam(':CATEGORY')));
-            
-        ?>
-    </pre>
+<section id="categories">
+    <div id="category-item">
+        <a href="#Produkter">
+            <p><?=$Categories[0]->categoryName?></p>
+            <img src="<?=Router::$BASE?>assets/media/<?=$Categories[0]->filename?>" alt="<?=$Categories[0]->categoryName?>">
+        </a>
+    </div>
+</section>
